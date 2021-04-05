5d804c9841269fb1278773d3c0d9d4263a339728
Ryan Conk <ryangconk@gmail.com>
2021-04-05 16:40:08 -0600

    add questions table

---
+++ app/models/questions.py
@@ -0,0 +1,8 @@
+from db import db
+
+class Questions(db.Model):
+    __tablename__ = 'questions'
+    id = db.Column(db.Integer, primary_key=True)
+    question = db.Column(db.Text, nullable=False)
+    answer = db.Column(db.Text, nullable=False)
+    subcategory = db.Column(db.String(250), nullable=False)
