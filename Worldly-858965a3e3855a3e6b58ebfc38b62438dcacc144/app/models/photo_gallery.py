21cb1e1dd0e6f2457cb9490067330da39cfbd0b2
Ryan Conk <ryangconk@gmail.com>
2021-04-05 16:17:55 -0600

    remove the extra file

--- Worldly-858965a3e3855a3e6b58ebfc38b62438dcacc144/app/models/photo_gallery.py
+++
@@ -1,9 +0,0 @@
-from .db import db
-
-class PhotoGallery(db.Model):
-    __tablename__ = 'photo_gallery'
-
-    id = db.Column(db.Integer, primary_key = True)
-    event_id = db.Column(db.Integer, db.ForeignKey("events.id"),nullable=False)
-    description = db.Column(db.Text, nullable=False)
-    url = db.Column(db.String(100), nullable = False)
