3ff83f6ed931d6b55689be48fcdbcc903bc2058e
Ryan Conk <ryangconk@gmail.com>
2021-04-13 10:02:49 -0600

    add more of the flashcards

--- react-app/src/components/MainPage/index.js
+++ react-app/src/components/MainPage/index.js
@@ -1,36 +1,60 @@
-import React, { useEffect, useState } from 'react'
-import styles from './styles.module.css'
+import React, { useEffect, useState } from "react";
+import {useHistory} from 'react-router-dom'
+import style from "react-syntax-highlighter/dist/esm/styles/hljs/a11y-dark";
+import styles from "./styles.module.css";
 
-const MainPage = () => {
+// TODO 
+const redirect = () => {
+        let repoId = 2
+        hisory.
 
-
-    return (
-        <>
-            <div>
-                {/* TODO add the banner */}
-                <div className={styles.banner} ></div>
-
-                {/* Grid  for 2 */}
-                <div className={styles.categories} >
-                    {/* Flex for recent */}
-                    <div className={styles.recentContainer} >
-                    
-                    
-                    </div>
-                    {/* Popular Topic */}
-                    <div className={styles.topics} >
-                    
-                    
-                    </div>
-                    
-                </div>
-            
-            
-            </div>
-  
-
-        </>
-    )
 }
 
-export default MainPage
+
+const MainPage = () => {
+	return (
+		<>
+			<div>
+				{/* TODO add the banner */}
+				<div className={styles.banner}></div>
+
+                
+
+				{/* Grid */}
+                <div className={styles.categories}>
+                    {/* Grid Item One */}
+                    <div className={styles.rightContainer} >
+                        {/* If the is no recent */}
+                        <div className={style.altContainer}>
+
+
+                        </div>
+                    {/*  */}
+
+                        {/* Else */}
+                    </div>
+
+
+
+
+
+                    {/* Grid Item Two  */}
+					<div className={styles.recentContainer}>
+						<div className={styles.topics}>
+							<div className={style.topicsContainer}>
+								<div className={styles.topicHeader}>Popular Subjects</div>
+								<div className={styles.linkContainer}>
+									<a>Python Methods</a>
+									<a>Interview Material</a>
+								</div>
+							</div>
+						</div>
+				    
+                    </div>
+				</div>
+			</div>
+		</>
+	);
+};
+
+export default MainPage;
