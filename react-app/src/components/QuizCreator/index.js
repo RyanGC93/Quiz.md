005a5c781899b25805461be1f7899be750ba0727
Ryan Conk <ryangconk@gmail.com>
2021-04-25 18:34:04 -0600

    made changes to the quiz creator to render a title

--- react-app/src/components/QuizCreator/index.js
+++ react-app/src/components/QuizCreator/index.js
@@ -1,111 +1,102 @@
 import React, { useEffect, useState } from "react";
 import styles from "./styles.module.css";
 import QuestionRow from "./QuestionRow";
-import {useHistory} from "react-router-dom"
+import { useHistory } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { getQuestions } from "../../store/questions";
 import { getRepos } from "../../store/repo";
 import { useParams } from "react-router-dom";
 import { createQuestion } from "../../store/questions";
 import { editRepo } from "../../store/repo";
-import {deleteRepo}  from "../../store/repo";
-import {IoAddCircle} from "react-icons/io5";
-import { MdDelete} from "react-icons/md";
+import { deleteRepo } from "../../store/repo";
+import { IoAddCircle } from "react-icons/io5";
+import { MdDelete } from "react-icons/md";
 
 const QuizCreator = () => {
-	const user = useSelector(state => state.session.user)
-	const [rowQuestion,setRowQuestion] =useState('')
-	const [rowAnswer, setRowAnswer] = useState('')
-	const [timedUpdate,setTimedUpdate] = useState(null)
-	const [title, setTitle] = useState('')
-	const history = useHistory()
-	
-
 	let para = useParams();
 	const dispatch = useDispatch();
+	const history = useHistory();
+
+	const [rowQuestion, setRowQuestion] = useState("");
+	const [rowAnswer, setRowAnswer] = useState("");
+	const [timedUpdate, setTimedUpdate] = useState(null);
+	const [repoTitle, setRepoTitle] = useState("");
+
+	const user = useSelector((state) => state.session.user);
 	const questions = useSelector((state) => Object.values(state.questions));
 	const repoInfo = useSelector((state) => {
-		if(state.repo[0])return Object.values(state.repo).filter((repository) => para.id === repository.repo_id)
+		return Object.values(state.repo).filter((repo) => para.id == repo.repo_id);
 	});
-	const [repoTitle,setRepoTitle] = useState('')
 
 	useEffect(() => {
 		if (!para.id) return;
 		if (!user) return;
+		if (!repoInfo) return;
 		// if(repoInfo && user.id !== repoInfo.owner_id) history.push(`/profile/${user.id}`)
-		if (repoInfo) return setRepoTitle(repoInfo.name)
+		console.log(repoInfo);
+		if (repoInfo[0]) setRepoTitle(repoInfo[0].name);
+		if (repoInfo[0] && questions) return;
 		if (para !== 0) {
 			dispatch(getQuestions(para.id));
-			dispatch(getRepos(user.id));			
+			dispatch(getRepos(user.id));
 		}
-		if (!repoInfo) return
-		setRepoTitle(repoInfo.name)
-		// if(para.owner_id !== user.id) history.push('/')
-	}, [user,para, repoInfo, dispatch]);
+	}, [user, para, dispatch]);
 
 	const updateTitle = (e) => {
-		setRepoTitle(e.target.value)
-		timedDataUpdate()
-	}
+		setRepoTitle(e.target.value);
+		console.log(repoTitle);
+		timedDataUpdate();
+	};
 	const timedDataUpdate = () => {
-		if(timedUpdate) clearTimeout(timedUpdate)
-		
+		if (timedUpdate) clearTimeout(timedUpdate);
+
 		const updateTime = 4000;
-		let timedSave =setTimeout(function(){
-			dispatch(editRepo(para.id,repoTitle))
+		let timedSave = setTimeout(function () {
+			dispatch(editRepo(para.id, repoTitle));
 		}, updateTime);
-		setTimedUpdate(()=>timedSave)
-	}
+		setTimedUpdate(() => timedSave);
+	};
 
 	const addQuestionHandler = () => {
-		if(!rowQuestion && !rowAnswer) return alert('Must add add question/answer')
-		dispatch(createQuestion(para.id, rowQuestion, rowAnswer))
-		setRowQuestion('')
-		setRowAnswer('')
-	}
+		if (!rowQuestion && !rowAnswer)
+			return alert("Must add add question/answer");
+		dispatch(createQuestion(para.id, rowQuestion, rowAnswer));
+		setRowQuestion("");
+		setRowAnswer("");
+	};
 
-	const answerHandler = (e) => {
-		setRowAnswer(e.target.value)
-	}
-
-	const questionHandler = (e) => {
-		setRowQuestion(e.target.value)
-	}
 	const removeRepo = () => {
-		let res = dispatch(deleteRepo(para.id))
-		if(!res.errors) history.push(`/profile/${user.id}`)
-	}
+		let res = dispatch(deleteRepo(para.id));
+		if (!res.errors) history.push(`/profile/${user.id}`);
+	};
 
-	if (!user && !repoInfo) return null
-	// if(repoInfo && user.id !== repoInfo.owner_id) history.push(`/profile/${user.id}`)
-
+	if (!user && !repoInfo) return null;
 	return (
 		<>
-			
-			{!repoInfo && (
-			null
-			)}
+			{!repoInfo && null}
 			<div className={styles.quizPage}>
 				<div className={styles.titleContainer}>
-						<div className={styles.title}>Title</div>
-						<input
-							type="text"
-							name="title"
-							  onChange={updateTitle}
-							  value={repoTitle}
+					<div className={styles.title}></div>
+					<input
+						type="text"
+						name="title"
+						onChange={(e) => updateTitle(e)}
+						value={repoTitle}
 					/>
-					<MdDelete onClick={removeRepo}  className={styles.icon}/>
-					</div>
+					<MdDelete onClick={removeRepo} className={styles.icon} />
+				</div>
 				<div className={styles.quizGrid}>
 					{/* iterate over the array */}
 					{questions[0] &&
 						questions.map((question) => <QuestionRow question={question} />)}
-					
+
 					{/* To add More */}
-					<div className={styles.inputTitle} >Create More</div> 
+					<div className={styles.inputTitle}>Create More</div>
 					<div className={styles.inputRow}>
 						<textarea
-							onChange={(e)=>{setRowQuestion(e.target.value)}}
+							onChange={(e) => {
+								setRowQuestion(e.target.value);
+							}}
 							className={styles.cell}
 							value={rowQuestion}
 						/>
@@ -115,8 +106,11 @@ const QuizCreator = () => {
 							value={rowAnswer}
 						/>
 					</div>
-					<div className={styles.addBtnContainer} >
-						< IoAddCircle className={styles.addIcon} onClick={addQuestionHandler} />
+					<div className={styles.addBtnContainer}>
+						<IoAddCircle
+							className={styles.addIcon}
+							onClick={addQuestionHandler}
+						/>
 					</div>
 				</div>
 			</div>
