158244476cb41dafbcc7e78a3cd02494efed797c
Ryan Conk <ryangconk@gmail.com>
2021-04-26 23:52:58 -0600

    add angel list and convert styling to css modules

--- react-app/src/components/LoginPage/PortfolioNavigator/index.js
+++ react-app/src/components/LoginPage/PortfolioNavigator/index.js
@@ -1,23 +1,36 @@
-import React from 'react'
-import "./styles.css";
-import {FaLinkedin} from 'react-icons/fa'
-import {FaGithubSquare} from 'react-icons/fa'
-import {SiGmail} from 'react-icons/si'
+import React from "react";
+import styles from "./styles.module.css";
+import { FaLinkedin } from "react-icons/fa";
+import { FaGithubSquare } from "react-icons/fa";
+import { SiGmail } from "react-icons/si";
+import { FaAngellist } from "react-icons/fa";
+
 export default function () {
-  
 
-
-  return (
-    <div>
-
-      <div className="professional-header">Professional Resources</div>
-        <div className="professional-row" >
-          <FaLinkedin className='professional-fa' onClick={() => window.location.href = 'https://www.linkedin.com/in/ryan-conk-8084b4103/'}/>
-        <FaGithubSquare className='professional-fa ' onClick={() => window.location.href = 'https://github.com/RyanGC93'} />
-        {/* TODO redirect to email */}
-          <SiGmail className='professional-fa' onClick={() => window.location.href = ''} />
-
-      </div>
-    </div>
-  )
+	return (
+		<div>
+			<div className={styles.professionalHeader}>Professional Resources</div>
+			<div className={styles.professionalRow}>
+				<FaLinkedin
+					className={styles.professionalFa}
+					onClick={() =>
+						(window.location.href =
+							"https://www.linkedin.com/in/ryan-conk-8084b4103/")
+					}
+				/>
+				<FaGithubSquare
+					className={styles.professionalFa}
+					onClick={() => (window.location.href = "https://github.com/RyanGC93")}
+				/>
+				{/* TODO redirect to email */}
+				<a href="mailto:ryangconk@gmail.com">
+					<SiGmail className={styles.professionalFa}  />
+				</a>
+        <FaAngellist
+					className={styles.professionalFa}
+					onClick={() => (window.location.href = "https://github.com/RyanGC93")}
+				/>
+			</div>
+		</div>
+	);
 }
