58948cdbe50eb89ba27c6373f7bfaa471f61f910
Ryan Conk <ryangconk@gmail.com>
2021-05-03 00:49:28 -0600

    added the preview to the markdown

--- react-app/src/components/QuizCreator/QuestionRow/index.js
+++ react-app/src/components/QuizCreator/QuestionRow/index.js
@@ -4,15 +4,33 @@ import { deleteQuestion } from "../../../store/questions";
 import { editQuestion } from "../../../store/questions";
 import { useDispatch } from "react-redux";
 import { MdDelete } from "react-icons/md";
+import { HiEye, HiEyeOff } from "react-icons/hi";
+import ReactMarkdown from "react-markdown";
+import gfm from "remark-gfm";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { atomDark } from "react-syntax-highlighter/dist/esm/styles/prism";
+
+const renderers = {
+	code: ({ language, value }) => {
+		return (
+			<SyntaxHighlighter
+				style={atomDark}
+				language={language}
+				children={value}
+			/>
+		);
+	},
+};
 
 const QuestionRow = ({ question, autoSaveDisplay }) => {
 	const dispatch = useDispatch();
 	const [rowQuestion, setRowQuestion] = useState("");
 	const [rowAnswer, setRowAnswer] = useState("");
 	const [timedUpdate, setTimedUpdate] = useState(null);
+	const [mdToggle, setMdToggle] = useState(false);
 
 	useEffect(() => {
-		if(rowQuestion && rowAnswer) return
+		if (rowQuestion && rowAnswer) return;
 		setRowQuestion(question.question);
 		setRowAnswer(question.answer);
 	});
@@ -51,20 +69,58 @@ const QuestionRow = ({ question, autoSaveDisplay }) => {
 			<div className={styles.inputRow}>
 				{/* question cell */}
 				<form className={styles.form}>
-					<textarea
-						className={styles.cell}
-						value={rowQuestion}
-						onChange={questionHandler}
-					></textarea>
+					{mdToggle ? (
+						<ReactMarkdown
+							renderers={renderers}
+							allowDangerousHtml={true}
+							source={rowQuestion}
+							plugins={[gfm]}
+							className={styles.cell}
+						>
+							{rowQuestion}
+						</ReactMarkdown>
+					) : (
+						<textarea
+							className={styles.cell}
+							value={rowQuestion}
+							onChange={questionHandler}
+						></textarea>
+					)}
 				</form>
 				{/* Answer Cell */}
 				<form className={styles.form}>
-					<textarea
-						className={styles.cell}
-						value={rowAnswer}
-						onChange={answerHandler}
-					></textarea>
-					<MdDelete onClick={deleteHandler} className={styles.icon} />
+					{mdToggle ? (
+						<ReactMarkdown
+							renderers={renderers}
+							allowDangerousHtml={true}
+							source={rowAnswer}
+							plugins={[gfm]}
+							className={styles.cell}
+						>
+							{rowAnswer}
+						</ReactMarkdown>
+					) : (
+						<textarea
+							className={styles.cell}
+							value={rowAnswer}
+							onChange={answerHandler}
+						></textarea>
+					)}
+
+					<div className={styles.iconContainer}>
+						<MdDelete onClick={deleteHandler} className={styles.icon} />
+						{mdToggle ? (
+							<HiEyeOff
+								className={styles.icon}
+								onClick={() => setMdToggle(!mdToggle)}
+							/>
+						) : (
+							<HiEye
+								className={styles.icon}
+								onClick={() => setMdToggle(!mdToggle)}
+							/>
+						)}
+					</div>
 				</form>
 			</div>
 		</>
