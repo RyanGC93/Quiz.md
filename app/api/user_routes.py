25e9669ae00f4a763da17384b512c5c5f7c67199
Ryan Conk <ryangconk@gmail.com>
2021-06-03 09:28:39 -0600

    add the last file on the sql error handling

--- app/api/user_routes.py
+++ app/api/user_routes.py
@@ -1,6 +1,8 @@
 from flask import Blueprint, jsonify
 from flask_login import login_required
 from app.models import User
+from sqlalchemy import exc
+
 
 user_routes = Blueprint('users', __name__)
 
@@ -8,12 +10,19 @@ user_routes = Blueprint('users', __name__)
 @user_routes.route('/')
 @login_required
 def users():
-    users = User.query.all()
-    return {"users": [user.to_dict() for user in users]}
-
+    try:    
+        users = User.query.all()
+        return {"users": [user.to_dict() for user in users]}
+    except exc.SQLAlchemyError as e:
+        print(type(e))
+        return {'errors': ['Cannot Get Users Please Try again']}, 500
 
 @user_routes.route('/<int:id>')
 @login_required
 def user(id):
-    user = User.query.get(id)
-    return user.to_dict()
+    try:
+        user = User.query.get(id)
+        return user.to_dict()
+    except exc.SQLAlchemyError as e:
+        print(type(e))
+        return {'errors': ['Cannot Get User Please Try again']}, 500
