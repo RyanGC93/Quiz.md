8428574cd14b11e3d8a04c94f8dc967e6b042f16
Ryan Conk <ryangconk@gmail.com>
2021-06-03 09:26:25 -0600

    add errorHandling to the search routes and removed unused file

--- app/api/search_routes.py
+++ app/api/search_routes.py
@@ -1,6 +1,8 @@
 from flask import Blueprint, jsonify
 from flask_login import login_required
 from app.models import db, QuestionsRepo
+from sqlalchemy import exc
+
 
 search_routes = Blueprint('search', __name__)
 
@@ -8,7 +10,11 @@ search_routes = Blueprint('search', __name__)
 @search_routes.route('/')
 @login_required
 def Events():
-    x = db.session.query(QuestionsRepo.id,QuestionsRepo.name).all()
-    keys = ['id', 'name']
-    search = {"search": [dict(zip(keys,values)) for values in x]}
-    return search
+    try:
+        x = db.session.query(QuestionsRepo.id,QuestionsRepo.name).all()
+        keys = ['id', 'name']
+        search = {"search": [dict(zip(keys,values)) for values in x]}
+        return search
+    except exc.SQLAlchemyError as e:
+        print(type(e))
+        return {'errors': ['Cannot Get Searched Repos Please Try again']}, 500
